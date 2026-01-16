8efcc5ba43d17def90f7f1c1357bfcd1c19b6355
Igo Machado <igo@dev.com>
2026-01-16 15:35:10 -0300

    Add Tela Principal e demais telas (para testes) ao PooTelSystemApplication

--- src/main/java/br/com/ifba/PooTelSystemApplication.java
+++ src/main/java/br/com/ifba/PooTelSystemApplication.java
@@ -1,7 +1,10 @@
 package br.com.ifba;
 
+import br.com.ifba.cliente.view.ListarClientes;
+import br.com.ifba.hotel.view.HotelLitar;
 import br.com.ifba.reserva.service.ReservaService;
 import br.com.ifba.reserva.view.TelaReserva;
+import br.com.ifba.view.TelaPrincipal;
 import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -13,22 +16,23 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class PooTelSystemApplication {
 
     public static void main(String[] args) {
-        FlatLightLaf.setup(); // Look and Feel moderno
-
-        UIManager.put("Component.arc", 12);
-        UIManager.put("Button.arc", 12);
-        UIManager.put("TextComponent.arc", 10);
-
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(PooTelSystemApplication.class)
-                .headless(false).run(args);
-
-        // pega o service do Spring
-        ReservaService reservaService = context.getBean(ReservaService.class);
-
-        // abre a tela principal passando o service
-        SwingUtilities.invokeLater(() -> {
-            TelaReserva tela = new TelaReserva(reservaService);
-            tela.setVisible(true);
-        });
+        FlatLightLaf.setup();//Ativa o Look and Feel moderno (ANTES do Spring criar os beans)
+            
+            //arredondam os componentes 
+            UIManager.put("Component.arc", 12);
+            UIManager.put("Button.arc", 12);
+            UIManager.put("TextComponent.arc", 10);
+            
+            ConfigurableApplicationContext context = new SpringApplicationBuilder(PooTelSystemApplication.class)
+              .headless(false).run(args); 
+            
+            //Abre a tela Swing na EDT (garante que vai abrir na thread correta)
+            SwingUtilities.invokeLater(() -> {
+            TelaPrincipal novaTela = context.getBean(TelaPrincipal.class);
+            //ListarClientes novaTela = context.getBean(ListarClientes.class);
+            //HotelLitar novaTela = context.getBean(HotelLitar.class);
+            //TelaReserva novaTela = context.getBean(TelaReserva.class);
+            novaTela.setVisible(true);
+            });
     }
 }
