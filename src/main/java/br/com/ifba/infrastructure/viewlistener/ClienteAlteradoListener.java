fbc6e321f1dd83bc0d8582539738225b0e3a2efa
raibeep <20242ireads0002@ifba.edu.br>
2026-01-29 15:26:16 -0300

    Adiciona listener para atualizar tabelas após serem alteradas

---
+++ src/main/java/br/com/ifba/infrastructure/viewlistener/ClienteAlteradoListener.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.infrastructure.viewlistener;
+
+
+@FunctionalInterface
+public interface ClienteAlteradoListener {
+    void onClienteAlterado();
+}
