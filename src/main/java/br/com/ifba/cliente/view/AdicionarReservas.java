f465f20bbf6148af8c0ce558a3c3392590909fde
Igo Machado <igo@dev.com>
2026-02-06 16:26:45 -0300

    Refactor adição do Jcalendar, hotel, quartos, preço diária

--- src/main/java/br/com/ifba/cliente/view/AdicionarReservas.java
+++ src/main/java/br/com/ifba/cliente/view/AdicionarReservas.java
@@ -5,15 +5,20 @@
 package br.com.ifba.cliente.view;
 
 import br.com.ifba.cliente.entity.Cliente;
-import br.com.ifba.infrastructure.util.Utils;
-import br.com.ifba.infrastructure.viewlistener.ReservaAlteradaListener;
+import br.com.ifba.hotel.controller.HotelIController;
+import br.com.ifba.hotel.entity.Hotel;
 import br.com.ifba.infrastructure.windowmanager.WindowManager;
+import br.com.ifba.quarto.entity.Quarto;
 import br.com.ifba.reserva.controller.ReservaIController;
 import br.com.ifba.reserva.entity.Reserva;
-import jakarta.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Locale;
+import javax.swing.JOptionPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,18 +30,40 @@ public class AdicionarReservas extends javax.swing.JFrame {
     
     @Autowired
     private WindowManager windowManager;
+    @Autowired
+    private HotelIController otelcontroller;
     
     private Cliente cliente;
+    
+    private Quarto quarto;
+    
+    private Hotel otel;
+    
+    private BigDecimal total;
     @Autowired
     private ReservaIController reservaIController;
     /*@Autowired
     private ReservaAlteradaListener reservaAlteradaListener;*/
-    public AdicionarReservas() {
+    public AdicionarReservas(HotelIController otelcontroller) {
+        this.windowManager =windowManager;
         initComponents();
     }
     
     public void init() {
         this.cliente = windowManager.getClienteSelecionado();
+        this.quarto = quarto;
+        this.otel = otel;
+        this.total = total;
+        
+        List<Hotel> hoteis = otelcontroller.findAll();
+
+        cbHotel.removeAllItems();
+        for (Hotel h : hoteis) {
+            cbHotel.addItem(h);
+        }     
+        cbQuarto.addActionListener(e -> atualizarPrecoTotal());
+        jcDataEntrada.addPropertyChangeListener("date", evt -> atualizarPrecoTotal());
+        jcDataSaida.addPropertyChangeListener("date", evt -> atualizarPrecoTotal());
     }
 
     @SuppressWarnings("unchecked")
@@ -44,35 +71,62 @@ public class AdicionarReservas extends javax.swing.JFrame {
     private void initComponents() {
 
         lblTitulo = new javax.swing.JLabel();
-        lblValorTotal = new javax.swing.JLabel();
         lblDataEntrada = new javax.swing.JLabel();
+        lblHotel = new javax.swing.JLabel();
+        btnSavarReserva = new javax.swing.JButton();
+        jcDataEntrada = new com.toedter.calendar.JDateChooser();
+        jcDataSaida = new com.toedter.calendar.JDateChooser();
         lblDataSaida = new javax.swing.JLabel();
-        txtValorTotal = new javax.swing.JTextField();
-        txtDataEntrada = new javax.swing.JTextField();
-        txtDataSaida = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        cbHotel = new javax.swing.JComboBox<>();
+        lblQuarto = new javax.swing.JLabel();
+        cbQuarto = new javax.swing.JComboBox<>();
+        lblPreco = new javax.swing.JLabel();
+        lblTotal = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(607, 607));
 
         lblTitulo.setFont(new java.awt.Font("Segoe UI Black", 1, 24)); // NOI18N
         lblTitulo.setText("Adicionar Reservas");
 
-        lblValorTotal.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        lblValorTotal.setText("Valor Total: ");
-
-        lblDataEntrada.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblDataEntrada.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
         lblDataEntrada.setText("Data de Entrada: ");
 
-        lblDataSaida.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        lblDataSaida.setText("Data de Saída: ");
+        lblHotel.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        lblHotel.setText("Hotel:");
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jButton1.setText("Adicionar Reserva");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnSavarReserva.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnSavarReserva.setText("Adicionar Reserva");
+        btnSavarReserva.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnSavarReservaActionPerformed(evt);
             }
         });
+
+        jcDataEntrada.setFont(new java.awt.Font("Liberation Sans", 0, 18)); // NOI18N
+
+        jcDataSaida.setFont(new java.awt.Font("Liberation Sans", 0, 18)); // NOI18N
+
+        lblDataSaida.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        lblDataSaida.setText("Data de Saída: ");
+
+        cbHotel.setFont(new java.awt.Font("Liberation Sans", 0, 18)); // NOI18N
+        cbHotel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbHotelActionPerformed(evt);
+            }
+        });
+
+        lblQuarto.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        lblQuarto.setText("Quarto:");
+
+        cbQuarto.setFont(new java.awt.Font("Liberation Sans", 0, 18)); // NOI18N
+
+        lblPreco.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        lblPreco.setText("Preço:");
+
+        lblTotal.setFont(new java.awt.Font("Liberation Sans", 0, 20)); // NOI18N
+        lblTotal.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -85,115 +139,123 @@ public class AdicionarReservas extends javax.swing.JFrame {
                         .addComponent(lblTitulo))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(36, 36, 36)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblValorTotal)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtValorTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 398, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblDataEntrada)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtDataEntrada))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblDataSaida)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtDataSaida)))))
-                .addContainerGap(85, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(221, 221, 221))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblDataEntrada)
+                            .addComponent(lblDataSaida)
+                            .addComponent(lblHotel)
+                            .addComponent(lblQuarto)
+                            .addComponent(lblPreco))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jcDataEntrada, javax.swing.GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)
+                                .addComponent(jcDataSaida, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(cbHotel, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(lblTotal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(cbQuarto, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(211, 211, 211)
+                        .addComponent(btnSavarReserva)))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(43, 43, 43)
                 .addComponent(lblTitulo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(42, 42, 42)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblDataEntrada)
+                            .addComponent(jcDataEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(36, 36, 36)
+                        .addComponent(jcDataSaida, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblDataSaida))
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblHotel)
+                    .addComponent(cbHotel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(43, 43, 43)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblValorTotal)
-                    .addComponent(txtValorTotal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
+                    .addComponent(lblQuarto)
+                    .addComponent(cbQuarto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblDataEntrada)
-                    .addComponent(txtDataEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(33, 33, 33)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblDataSaida)
-                    .addComponent(txtDataSaida, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(58, 58, 58)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(93, Short.MAX_VALUE))
+                    .addComponent(lblPreco)
+                    .addComponent(lblTotal))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 95, Short.MAX_VALUE)
+                .addComponent(btnSavarReserva, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        if (cliente == null) {
-            Utils.mostrarErro(this, "Cliente não informado.");
-            return;
-        }  
-        String valorTxt = txtValorTotal.getText().trim();
-        String dataEntradaTxt = txtDataEntrada.getText().trim();
-        String dataSaidaTxt = txtDataSaida.getText().trim();
-
-        if (valorTxt.isEmpty() || dataEntradaTxt.isEmpty() || dataSaidaTxt.isEmpty()) {
-            Utils.mostrarAviso(this, "Preencha todos os campos.");
-            return;
-        }
-
-        double valorTotal;
+    
+    private void atualizarPrecoTotal() {
         try {
-            valorTotal = Double.parseDouble(valorTxt.replace(",", "."));
-        } catch (NumberFormatException e) {
-            Utils.mostrarErro(this, "Valor total inválido.");
-            return;
+            if (jcDataEntrada.getDate() != null && jcDataSaida.getDate() != null && cbQuarto.getSelectedItem() != null) {
+                LocalDate entrada = jcDataEntrada.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                LocalDate saida = jcDataSaida.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                Quarto q = (Quarto) cbQuarto.getSelectedItem();
+
+                if (saida.isAfter(entrada)) {
+                    long dias = ChronoUnit.DAYS.between(entrada, saida);
+                    if (dias == 0) {
+                        dias = 1;
+                    }
+                    total = q.getPrecoDiaria().multiply(new BigDecimal(dias));
+                    NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+                    lblTotal.setText(nf.format(total));
+                } else {
+                    lblTotal.setText("Data Inválida");
+                }
+            }
+        } catch (Exception e) {
+            lblTotal.setText("---");
         }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        LocalDate dataEntrada;
-        LocalDate dataSaida;
-
+    }
+    private void btnSavarReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSavarReservaActionPerformed
         try {
-            dataEntrada = LocalDate.parse(dataEntradaTxt, formatter);
-            dataSaida = LocalDate.parse(dataSaidaTxt, formatter);
-        } catch (DateTimeParseException e) {
-            Utils.mostrarErro(this, "Data inválida. Use o formato dd/MM/yyyy.");
-            return;
-        }
+            Reserva reserva = new Reserva();
+            reserva.setDataEntrada(jcDataEntrada.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+            reserva.setDataSaida(jcDataSaida.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+            reserva.setQuarto((Quarto) cbQuarto.getSelectedItem());
+            reserva.setCliente(this.cliente);
+            reserva.setHotel(this.otel);
+            reserva.setStatus(true);
+           
+            reserva.setValorTotal(total);
 
-        Reserva reserva = new Reserva();
-        //reserva.setValorTotal(valorTotal);
-        reserva.setDataEntrada(dataEntrada);
-        reserva.setDataSaida(dataSaida);
-        reserva.setStatus(true);
-        reserva.setCliente(cliente);
-
-        try {
             reservaIController.save(reserva);
-            /*if (reservaAlteradaListener != null) {
-                reservaAlteradaListener.onReservaAlterada();
-            }*/
+            JOptionPane.showMessageDialog(this, "Reserva Salva com Sucesso!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Erro ao salvar: " + e.getMessage());
+        }
+    }//GEN-LAST:event_btnSavarReservaActionPerformed
 
-            Utils.mostrarSucesso(this, "Reserva cadastrada com sucesso!");
-            dispose();
-
-        } catch (RuntimeException e) {
-            Utils.mostrarErro(this,
-            "Erro ao cadastrar reserva: " + e.getMessage());
-}
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void cbHotelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbHotelActionPerformed
+        // TODO add your handling code here:
+        otel = (Hotel) cbHotel.getSelectedItem();
+        cbQuarto.removeAllItems();
+        if (otel != null) {
+            List<Quarto> quartos = otelcontroller.findQuartosByHotel(otel);
+            for (Quarto q : quartos) cbQuarto.addItem(q);
+        }
+    }//GEN-LAST:event_cbHotelActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnSavarReserva;
+    private javax.swing.JComboBox<Hotel> cbHotel;
+    private javax.swing.JComboBox<Quarto> cbQuarto;
+    private com.toedter.calendar.JDateChooser jcDataEntrada;
+    private com.toedter.calendar.JDateChooser jcDataSaida;
     private javax.swing.JLabel lblDataEntrada;
     private javax.swing.JLabel lblDataSaida;
+    private javax.swing.JLabel lblHotel;
+    private javax.swing.JLabel lblPreco;
+    private javax.swing.JLabel lblQuarto;
     private javax.swing.JLabel lblTitulo;
-    private javax.swing.JLabel lblValorTotal;
-    private javax.swing.JTextField txtDataEntrada;
-    private javax.swing.JTextField txtDataSaida;
-    private javax.swing.JTextField txtValorTotal;
+    private javax.swing.JLabel lblTotal;
     // End of variables declaration//GEN-END:variables
 }
